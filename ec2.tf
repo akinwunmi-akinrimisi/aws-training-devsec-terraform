46e84b9a379d697d00829bd5dc848445e82b7635
Akinwunmi Akinrimisi <78056848+akinwunmi-akinrimisi@users.noreply.github.com>
2025-04-17 12:10:30 +0100

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -3,6 +3,34 @@ resource "aws_instance" "web" {
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.main.id
   vpc_security_group_ids = [aws_security_group.sg.id]
+  user_data = base64encode(<<-EOF
+              #!/bin/bash
+              # Update the instance and install necessary packages
+              yum update -y
+              yum install -y httpd wget unzip
+              
+              # Start Apache and enable it to start on boot
+              systemctl start httpd
+              systemctl enable httpd
+              
+              # Navigate to the web root directory
+              cd /var/www/html
+              
+              # Download a CSS template directly
+              wget https://www.free-css.com/assets/files/free-css-templates/download/page284/built-better.zip
+              
+              # Unzip the template and move the files to the web root
+              unzip built-better.zip -d /var/www/html/
+              mv /var/www/html/html/* /var/www/html/
+              
+              # Clean up unnecessary files
+              rm -r /var/www/html/html
+              rm built-better.zip
+              
+              # Restart Apache to apply changes
+              systemctl restart httpd
+              EOF
+  )
 
   tags = {
     Name = "main-instance"

ec17235f6b354de174af26726c69d0ce1f57d152
Akinwunmi Akinrimisi <78056848+akinwunmi-akinrimisi@users.noreply.github.com>
2025-06-25 01:46:34 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "<your-unique-bucket-name>" #replace with your bucket name
     key            = "terraform/state.tfstate"
     region         = "<your-region>" #replace with the right region
-    dynamodb_table = "terraform-locks"
+    dynamodb_table = "terraform-locks-YOURNAME"  #replace with the right name
     encrypt        = true
   }
 }
